73c7b42da5e9c46226164efbdab42b88b8f0199a
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-20 17:29:24 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :artists, only: [:new, :create, :show, :edit, :update]
+  resources :songs, only: [:index, :new, :create, :show, :edit, :update]
+  resources :genres, only: [:new, :create, :show, :edit, :update]
+  
 end
